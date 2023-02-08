fcb2df9a53520e69d7fcf9e87d5357b5d9c3c8d2
testone <@>
2023-02-08 23:59:01 +0200

    add edit in xml login fragment

--- app/src/main/java/com/example/taskapp/LoginFragment.kt
+++ app/src/main/java/com/example/taskapp/LoginFragment.kt
@@ -5,9 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.taskapp.databinding.FragmentLoginBinding
 
 
 class LoginFragment : Fragment() {
+
+    private lateinit var binding : FragmentLoginBinding
 
 
     override fun onCreateView(
@@ -15,7 +19,8 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_login,container,false)
+        return binding.root
     }
 
 
