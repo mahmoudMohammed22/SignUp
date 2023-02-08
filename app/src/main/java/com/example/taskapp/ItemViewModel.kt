45aec3a16e8e94674a0d4208ae6a91c511d5ec28
testone <@>
2023-02-09 00:35:35 +0200

    add view Model

---
+++ app/src/main/java/com/example/taskapp/ItemViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.taskapp
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ItemViewModel : ViewModel() {
+
+    val firstName = MutableLiveData<String>()
+    val lastName = MutableLiveData<String>()
+    val email = MutableLiveData<String>()
+    val phoneNumber = MutableLiveData<Int>()
+
+
+}
