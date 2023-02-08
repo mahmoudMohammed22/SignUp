4beeb9dd618371ed824c0dfe5f392f3edb7bca73
testone <@>
2023-02-09 00:31:56 +0200

    add navigation in details Fragment

--- app/src/main/java/com/example/taskapp/DetailsFragment.kt
+++ app/src/main/java/com/example/taskapp/DetailsFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.example.taskapp.databinding.FragmentDetailsBinding
 import com.example.taskapp.databinding.FragmentLoginBinding
 
@@ -21,6 +22,14 @@ class DetailsFragment : Fragment() {
     ): View? {
         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_details,container,false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.Back.setOnClickListener {
+            val action = DetailsFragmentDirections.actionDetailsFragmentToLoginFragment()
+            findNavController().navigate(action)
+        }
     }
 
 
