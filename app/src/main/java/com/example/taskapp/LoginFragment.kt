f826ad92a66bd3650c8254080da9b476dfa19252
testone <@>
2023-02-09 00:25:52 +0200

    add  naivgation in fragment

--- app/src/main/java/com/example/taskapp/LoginFragment.kt
+++ app/src/main/java/com/example/taskapp/LoginFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.example.taskapp.databinding.FragmentLoginBinding
 
 
@@ -21,6 +22,15 @@ class LoginFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_login,container,false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.signUp.setOnClickListener {
+            val action = LoginFragmentDirections.actionLoginFragmentToDetailsFragment()
+            findNavController().navigate(action)
+        }
     }
 
 
